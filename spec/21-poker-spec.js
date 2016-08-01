a687e89ee4dfa60511dc335579745bafb41db4a9
Liuhaiyan6 <2391348695@qq.com>
2016-08-01 20:46:13 +0800

    commit

--- spec/21-poker-spec.js
+++ spec/21-poker-spec.js
@@ -2,165 +2,181 @@
  * Created by amber on 16-7-29.
  */
 'use strict';
-let {dealString,  otherSummations, dealA,comparePokerAB} =require("../src/21-point.js");
+let {sumPart, dealA, comparePokerAB} =require("../src/21-point.js");
 
-describe('dealString1', function () {
-    let inputs1 = "1-2-3-A-k";
-    it('dealString1', function () {
-        let charArray1 = dealString(inputs1);
-        expect(charArray1).toEqual(['1', '2', '3', 'A', 'k']);
+
+describe('#1 sumPart', function () {
+    it('sumPart', function () {
+        let inputs = ['2','5','9'];
+        let sumParts = sumPart(inputs);
+        let printParts=16;
+        expect(sumParts).toEqual(printParts);
+    })
+});
+describe('#2 sumPart', function () {
+    it('sumPart', function () {
+        let inputs = ['J','Q','K'];
+        let sumParts = sumPart(inputs);
+        let printParts=30;
+        expect(sumParts).toEqual(printParts);
+    })
+});
+
+describe('#3 sumPart', function () {
+    it('sumPart', function () {
+        let inputs = ['A','A','A','A'];
+        let sumParts = sumPart(inputs);
+        let printParts=0;
+        expect(sumParts).toEqual(printParts);
+    })
+});
+describe('#4 sumPart', function () {
+    it('sumPart', function () {
+        let inputs = ['2','J','A'];
+        let sumParts = sumPart(inputs);
+        let printParts=12;
+
+        expect(sumParts).toEqual(printParts);
+    })
+});
+describe('#5sumPart', function () {
+    it('sumPart', function () {
+        let inputs = ['A','A','2','Q'];
+        let sumParts = sumPart(inputs);
+        let printParts=12;
+        expect(sumParts).toEqual(printParts);
+    })
+});
+describe('#6 sumPart', function () {
+    it('sumPart', function () {
+        let inputs = ['A','Q','K'];
+        let sumParts = sumPart(inputs);
+        let printParts=20;
+        expect(sumParts).toEqual(printParts);
     })
 });
 
 
-describe('dealString2', function () {
-    let inputs2 = "1-2-A-k";
-    xit('dealString2', function () {
-        let charArray2 = dealString(inputs2);
-        expect(charArray2).toEqual(['1', '2', 'A', 'k']);
+
+describe('#1 dealA1', function () {
+
+    it('dealA1', function () {
+        let sumPart = 16;
+        let inputs = ['2','5','9'];
+        let sum = dealA(inputs, sumPart);
+        let expected =16;
+        expect(sum).toEqual(expected);
+    })
+});
+describe('#2 dealA1', function () {
+    it('dealA1', function () {
+        let sumPart = 30;
+        let inputs = ['J','Q','K'];
+        let sum = dealA(inputs, sumPart);
+        let expected =30;
+        expect(sum).toEqual(expected);
+    })
+});
+describe('#3 dealA1', function () {
+    it('dealA1', function () {
+        let sumPart = 0;
+        let inputs = ['A','A','A','A'];
+        let sum = dealA(inputs, sumPart);
+        let expected =14;
+        expect(sum).toEqual(expected);
+    })
+});
+describe('#4 1dealA1', function () {
+    it('dealA1', function () {
+        let sumPart = 12;
+        let inputs = ['2','J','A'];
+        let sum = dealA(inputs, sumPart);
+        let expected =13;
+        expect(sum).toEqual(expected);
+    })
+});describe('#5 dealA1', function () {
+
+    it('dealA1', function () {
+        let sumPart = 12;
+        let inputs = ['A','A','2','Q'];
+        let sum = dealA(inputs, sumPart);
+        let expected =14;
+
+        expect(sum).toEqual(expected);
+    })
+});
+describe('#6 dealA1', function () {
+    it('dealA1', function () {
+        let sumPart = 20;
+        let inputs = ['A','Q','K'];
+        let sum = dealA(inputs, sumPart);
+        let expected =21;
+        expect(sum).toEqual(expected);
     })
 });
 
 
-describe('sumPart1', function () {
-    let charArray1 = ['1', '2', '3', 'A', 'k'];
-    it('sumPart1', function () {
-        let sumPart1 = otherSummations(charArray1);
-        expect(sumPart1).toEqual(16);
-    })
-});
-describe('sumPart2', function () {
-    let charArray2 = ['1', '2', 'A', 'k'];
-    it('sumPart2', function () {
-        let sumPart2 = otherSummations(charArray2);
-        expect(sumPart2).toEqual(13);
+
+describe('#1 comparePokerAB', function () {
+    it('comparePokerAB', function () {
+        let sum1 = 43;
+        let sum2 = 43;
+        let winner = comparePokerAB(sum1, sum2);
+        expect(winner).toEqual('A and B are drawer');
     })
 });
 
-describe('dealA1', function () {
-    let sumPart1 = 16;
-    let charArray1 = ['1', '2', '3', 'A', 'k'];
-    fit('dealA1', function () {
-        let sum1 = dealA(charArray1, sumPart1);
-        expect(sum1).toEqual(17);
-    })
-});
-describe('dealA2', function () {
-    let sumPart2 = 13;
-    let charArray2 = ['1', '2', 'A', 'k'];
-    fit('dealA2', function () {
-        let  sum2 = dealA(charArray2, sumPart2);
-        expect(sum2).toEqual(14);
-    })
-});
-describe('comparePokerAB',function () {
-    let inputs1 = "1-2-3-A-k";
-    let inputs2 = "1-2-A-k";
-    let sum1 = 17;
-    let sum2 = 14;
-    it('comparePokerAB',function () {
-        let winner = comparePokerAB(sum1, sum2, inputs1, inputs2);
-        expect(winner).toEqual('A is winner');
+
+
+
+describe('#2 comparePokerAB', function () {
+
+    let sum1 = 22;
+    let sum2 = 23;
+    it('comparePokerAB', function () {
+        let winner = comparePokerAB(sum1, sum2);
+        expect(winner).toEqual('A and B are drawer');
     })
 });
 
-describe('comparePokerAB',function () {
-    let inputs1 = "1-2-3-A-k";
-    let inputs2 = "1-2-A-k";
-    let sum1 = 17;
-    let sum2 = 14;
-    it('comparePokerAB',function () {
-        let winner = comparePokerAB(sum1, sum2, inputs1, inputs2);
-        expect(winner).toEqual('A is winner');
-    })
-});
+describe('#3 comparePokerAB', function () {
 
-describe('comparePokerAB',function () {
-    let inputs1 = "A";
-    let inputs2 = "A-A-A";
-    let sum1 = 11;
-    let sum2 = 13;
-    it('comparePokerAB',function () {
-        let winner = comparePokerAB(sum1, sum2, inputs1, inputs2);
+    let sum1 = 18;
+    let sum2 = 19;
+    it('comparePokerAB', function () {
+        let winner = comparePokerAB(sum1, sum2);
         expect(winner).toEqual('B is winner');
     })
 });
-describe('comparePokerAB',function () {
-    let inputs1 = "5-7-9";
-    let inputs2 = "6-7-10";
-    let sum1 = 18;
-    let sum2 = 23;
-    it('comparePokerAB',function () {
-        let winner = comparePokerAB(sum1, sum2, inputs1, inputs2);
-        expect(winner).toEqual('A is winner');
-    })
-});
 
-describe('comparePokerAB',function () {
-    let inputs1 = "J-2-6";
-    let inputs2 = "J-2-2-4";
+describe('#4 comparePokerAB', function () {
+
     let sum1 = 18;
     let sum2 = 18;
-    it('comparePokerAB',function () {
-        let winner = comparePokerAB(sum1, sum2, inputs1, inputs2);
-        expect(winner).toEqual('A is winner');
-    })
-});
-describe('comparePokerAB',function () {
-    let inputs1 = "J-A-k";
-    let inputs2 = "A-J";
-    let sum1 = 21;
-    let sum2 =21;
-    it('comparePokerAB',function () {
-        let winner = comparePokerAB(sum1, sum2, inputs1, inputs2);
-        expect(winner).toEqual('A and B is drawer');
-    })
-});
-
-describe('comparePokerAB',function () {
-    let inputs1 = "10-A-1";
-    let inputs2 = "A-10-10";
-    let sum1 = 12;
-    let sum2 =21;
-    it('comparePokerAB',function () {
-        let winner = comparePokerAB(sum1, sum2, inputs1, inputs2);
-        expect(winner).toEqual('B is winner');
-    })
-});
-
-describe('comparePokerAB',function () {
-    let inputs1 = "10-A-1";
-    let inputs2 = "A-10-10";
-    let sum1 = 12;
-    let sum2 =21;
-    it('comparePokerAB',function () {
-        let winner = comparePokerAB(sum1, sum2, inputs1, inputs2);
-        expect(winner).toEqual('B is winner');
-    })
-});
-
-describe('comparePokerAB',function () {
-    let inputs1 = "9-A-1-1";
-    let inputs2 = "A-10-1";
-    let sum1 = 12;
-    let sum2 =21;
-    it('comparePokerAB',function () {
-        let winner = comparePokerAB(sum1, sum2, inputs1, inputs2);
+    let inputs11 =['2','9','7'];
+    let inputs12=['2','6','3','7'];
+    it('comparePokerAB', function () {
+        let winner =comparePokerAB(sum1, sum2, inputs11, inputs12);
         expect(winner).toEqual('A is winner');
     })
 });
 
 
-describe('comparePokerAB',function () {
-    let inputs1 = "9-J-Q";
-    let inputs2 = "9-K-Q";
-    let sum1 = 29;
-    let sum2 =29;
-    it('comparePokerAB',function () {
-        let winner = comparePokerAB(sum1, sum2, inputs1, inputs2);
-        expect(winner).toEqual('A and B is drawer');
+describe('#5 comparePokerAB', function () {
+
+    let sum1 = 22;
+    let sum2 = 20;
+
+    it('comparePokerAB', function () {
+        let winner = comparePokerAB(sum1, sum2);
+        expect(winner).toEqual('B is winner');
     })
 });
+
+
+
+
+
 
 
 
--- src/21-point.js
+++ src/21-point.js
@@ -1,105 +1,121 @@
 /**
  * Created by amber on 16-7-29.
  */
+
 let _ = require('lodash');
 
 
-function winner(inputs1, inputs2) {
+function winner(inputs11, inputs12) {
 
-    let charArray1 = dealString(inputs1);
-    let charArray2 = dealString(inputs2);
 
-    let sumPart1 = otherSummations(charArray1);
-    let sumPart2 = otherSummations(charArray2);
+    let sumPart1 = sumPart(inputs1);
+    let sumPart2 = sumPart(inputs2);
 
-    let sum1 = dealA(charArray2, sumPart1);
-    let sum2 = dealA(charArray2, sumPart2);
+    let sum1 = dealA(inputs1, sumPart1);
+    let sum2 = dealA(inputs2, sumPart2);
 
-    let Winner = comparePokerAB(sum1, sum2);
-    return Winner;
+    let winner = comparePokerAB(sum1, sum2, inputs11, inputs12);
+    return winner;
 }
 
-function dealString(inputs) {
-    return charArray = _.split(inputs, '-');
+function sumPart(inputs) {
+    let a = _.chain(inputs)
+        .filter(x=> x !== 'A')
+        .map(x=> {
+            if (x === 'J' || x === 'Q' || x === 'K') {
+                return '10';
+            }
+            else {
+                return x;
+            }
+        })
+        .value();
+
+    let b = _(a).map(x=> parseInt(x))
+
+        .sum();
+    // console.log(b);
+    return b;
 
 
-    function otherSummations(charArray) {
-        let a = _.chain(charArray).filter(x=> x !== 'A')
-            .map(x=> {
-                if (x = ['J', 'Q', 'K']) {
-                    return 10;
-                }
-                else {
-                    return x;
-                }
-            }).value();
-        return sumPart = _(a).map(x=> _.parseInt(x)).sum();
-    }
 }
 
 
-function dealA(charArray, sumPart) {
-    let a = _.chain(charArray);
-    _.filter(x=>x === 'A')
+function dealA(inputs, sumPart) {
+    let a = _.chain(inputs)
+        .filter(x=>x === 'A')
         .value();
     let lengthA = a.length;
     let partA = 21 - sumPart;
+    let sumPartA;
+    let sum;
     if (partA < 0) {
-        return 'over!';
+        return sumPart + lengthA;
+    }
+//       console.log(sum);
+//       console.log(sumPartA);
+    if (_.floor(partA / 11) !== 0) {
+        // sumPartA = lengthA;
+        // sum = sumPart + sumPartA;
+        // //  console.log(sum);
+        // return sum;
+        sumPartA = 11 + (lengthA - 1);
+        sum = sumPart + sumPartA;
+        return sum;
+
     } else {
-        let sumPartA;
-        let sum1;
-        if (_.floor(partA % 11 === 0)) {
-            sumPartA = lengthA1 + 1;
-            sum = sumPart + sumPartA;
-            return sum;
-        } else {
-            sumPartA = 11 + (lengthA - 1);
-            sum = partSum + sumPartA;
-            return sum;
-        }
+        sumPartA = lengthA;
+        sum = sumPart + sumPartA;
+        //  console.log(sum);
+        return sum;
+
+        // sumPartA = 11 + (lengthA - 1);
+        // sum = sumPart + sumPartA;
+        // return sum;
     }
 }
 
 
 function comparePokerAB(sum1, sum2, inputs1, inputs2) {
+
+
     if (sum1 > 21 && sum2 > 21) {
         return 'A and B are drawer';
-    } else {
-        if (sum1 === sum2 && inputs1.length > inputs2.length) {
-            return 'A is winner'
-        }
-        if (sum1 === sum2 && inputs1.length < inputs2.length) {
-            return 'B is winner'
-        }
-        if (sum1 === sum2 && inputs1.length === inputs2.length) {
-            return 'A and B are drawer';
-        } else {
-            if (sum1 < sum2 && inputs1.length < inputs2.length) {
-                return 'B is winner';
-
-            }
-            if (sum1 < sum2 && inputs1.length > inputs2.length) {
-                return 'B is winner';
-
-            }
-            if (sum1 > sum2 && inputs1.length > inputs2.length) {
-                return 'A is winner';
-            }
-            if (sum1 > sum2 && inputs1.length < inputs2.length) {
-                return 'B is winner';
-            }
-            else {
-                return 'A is winner';
-            }
-        }
-
     }
+
+    else if (sum1 === sum2 && inputs1.length > inputs2.length) {
+        return 'A is winner'
+    }
+    else if (sum1 === sum2 && inputs1.length < inputs2.length) {
+        return 'A is winner'
+    }
+    else if (sum1 === sum2 && inputs1.length === inputs2.length) {
+        return 'A and B are drawer';
+    }
+    else if (sum1 < sum2 && sum1 <= 21 && sum2 <= 21) {
+        return 'B is winner';
+    }
+    else if (sum1 > sum2 && sum1 <= 21 && sum2 <= 21) {
+        return 'A is winner';
+    }
+    else if (sum1 === sum2 && inputs1.length < inputs2.length) {
+        return 'A is winner';
+    }
+    else if (sum1 === sum2 && inputs1.length > inputs2.length) {
+        return 'B is winner';
+    }
+    else if (sum1 > 21 && sum2 <= 21) {
+        return 'B is winner'
+    }
+    if (sum1 <= 21 && sum2 > 21) {
+        return "A is winner"
+    }
+
+
 }
 
 module.exports = {
-    dealString: dealString,
-    otherSummations: otherSummations,
+    sumPart: sumPart,
     dealA: dealA,
     comparePokerAB: comparePokerAB,
 
